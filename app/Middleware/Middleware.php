cb5755c86977322d01221391ed2f6970f71bb64a
임성준 <zkfmak10@gmail.com>
2023-09-05 21:42:23 +0900

    edit constructor

--- app/Middleware/Middleware.php
+++ app/Middleware/Middleware.php
@@ -2,18 +2,24 @@
 
 namespace app\Middleware;
 
+use app\Controllers\Router;
+
 class Middleware
 {
+    private $router = '';
     // 사용할 미들웨어 클래스 등록
     // key => class
     protected $middlewares = [];
     private $middleware = '';
 
+
     /**
+     * @param $router Router
      * @param $middleware
      */
-    public function __construct($middleware)
+    public function __construct($router, $middleware)
     {
+        $this->router = $router;
         $this->middleware = $this->middlewares[$middleware];
     }
 }
