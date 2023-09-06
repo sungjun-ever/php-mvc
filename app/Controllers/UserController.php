74a098ef87f04077360783f2235f5621d00a017b
sungjun-ever <zkfmak10@gmail.com>
2023-09-06 22:33:47 +0900

    UserController

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -13,25 +13,20 @@ class UserController
 
     public function post(Request $request)
     {
-        print_r($request->all());
-//        print_r($request->getBody());
+        print_r($request->getBody());
 //        header("Location:http://localhost:3000/users/1");
     }
 
     public function show(Request $request)
     {
-        print_r($request->all());
-//        foreach ((array)$request as $key => $value) {
-//            print_r($key);
-//            echo '<br>';
-//            print_r($value);
-//            echo '<br>';
-//        }
+        print_r($request->getBody());
         include 'views/users/show.php';
     }
 
     public function edit(Request $request)
     {
+        print_r($request->getBody());
         include 'views/users/edit.php';
     }
+
 }
